7cea5ca504029976565adfcd175424bc2edb41fa
= <=>
2016-12-07 21:38:56 -0700

    Change to Vagrant

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+echo "Updating"
+sudo apt-get update
+
+echo "Installing"
+sudo apt-get install -y flex byacc bison gcc libusb-1.0-0 libusb-1.0-0-dev libc6-dev curl build-essential gcc-avr binutils-avr avr-libc gdb-avr avrdude vim
